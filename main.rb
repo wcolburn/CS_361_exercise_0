2ef089f4ee2889b9f5b6f9c1c242abff9142c998
Liam Colburn <liam002@thecolburns.us>
2023-09-27 11:28:56 -0700

    Add comment header and reformat for loop

--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
+# William Colburn
+# CS_361
+# Program prints out the square root of integers between 1 and 1000
+
 # frozen_string_literal: true
 
-for i in 1...1001 do
+(1...1001).each { |i|
   puts Math.sqrt(i)
-end
+}
